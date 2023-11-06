64502f7579763c5e30ddc8e9a418f9b5bb170b5f
SP RAKSHITH <rakshith0908@gmail.com>
2023-11-06 11:04:55 +0100

    Update Submodules

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from data_preprocessing import data_segregation
 
 FILL_MISSING_LOCATION = False  # This runs OpenAI API - Takes a lot of time to complete
 FETCH_LAT_LON = False  # This runs Google Maps API - Which Costs Money
-RECREATE_TABLES = False  # This will Trucate the tables in the BigQuery and Recreate them. If no change set to False.
+RECREATE_TABLES = True  # This will Trucate the tables in the BigQuery and Recreate them. If no change set to False.
 
 data_segregation.segregate_raw_dataset(FILL_MISSING_LOCATION, FETCH_LAT_LON)
 load_to_bigquery.create_datasets_and_tables(RECREATE_TABLES)
