b580383f6a3c0a55767492cf85b0048adfc0b1f0
praveenbm1997 <119062495+praveenbm1997@users.noreply.github.com>
2023-11-14 01:42:48 +0100

    added the function to extarct_all_data

--- extract_all_data.py
+++ extract_all_data.py
@@ -1,6 +1,6 @@
 from data_extraction.every_earthquake import extract_load_earthquake_data
 from data_loading.load_to_bigquery import load_extreme_temp_locations_forecast
-
+from data_extraction.severe_climate_alert import job
 # Code to Extract Earthquake Data and Load it to BigQuery
 try:
     extract_load_earthquake_data()
@@ -15,8 +15,6 @@ except Exception as e:
 
 # Code to Extract Severe Weather Data and Load it to BigQuery
 try:
-    # TODO: Extract and Load Severe Weather Data
-    # Write your code here
-    pass
+    job()
 except Exception as e:
     print(e)
