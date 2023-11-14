0598ee071c4258d8fce9bec570a58acac6643cd2
SP RAKSHITH <rakshith0908@gmail.com>
2023-11-14 02:40:37 +0100

    Updated CRON Job

--- extract_all_data.py
+++ extract_all_data.py
@@ -8,7 +8,8 @@ from data_loading.load_to_bigquery import load_extreme_temp_locations_forecast
 
 # Code to Load Historical Disaster Data to BigQuery
 try:
-    # The below function call will Clean, Extract Mission Locations(Lat and Lon) using OpenAI and GoogleMaps GeoCoding API.
+    # The below function call will Clean, Extract Mission Locations(Lat and Lon) using OpenAI and GoogleMaps
+    # GeoCoding API.
     FILL_MISSING_LOCATION = False  # This runs OpenAI API - Takes a lot of time to complete
     FETCH_LAT_LON = False  # This runs Google Maps API - Which Costs Money
     RECREATE_TABLES = False  # This will Trucate the tables in the BigQuery and Recreate them. If there is no change, set it to False
@@ -20,27 +21,27 @@ try:
 
     data_segregation.segregate_raw_dataset(FILL_MISSING_LOCATION, FETCH_LAT_LON)
     load_to_bigquery.create_datasets_and_tables(RECREATE_TABLES)
-
-    # The below function will fetch all the latest Earthquakes past month and load it to BigQuery.
-    # This function will be set to run once a day using Cron Job.
-    extract_load_earthquake_data()
+    print('Preprocessed and Loaded Historical Disaster Data to BigQuery')
 except Exception as e:
     print(e)
 
 # Code to Extract Earthquake Data and Load it to BigQuery
 try:
-    extract_load_earthquake_data()
+    extract_load_earthquake_data(load_all_at_once=False)
+    print('Extracted and Loaded Earthquake Data to BigQuery')
 except Exception as e:
     print(e)
 
 # Code to Extract Weather Data and Load it to BigQuery
 try:
     load_extreme_temp_locations_forecast()
+    print('Extracted and Loaded Forecast Data to BigQuery')
 except Exception as e:
     print(e)
 
 # Code to Extract Severe Weather Data and Load it to BigQuery
 try:
     severe_climate_extraction_job()
+    print('Extracted and Loaded Severe Climate Data to BigQuery')
 except Exception as e:
     print(e)
